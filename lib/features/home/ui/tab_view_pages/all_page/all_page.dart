ceb26e8942f3bd0411f996433164570b558ca7c8
Shebin-shaji <shebinshaji2615@gmail.com>
2024-06-01 00:26:44 +0530

    tabs completed

--- lib/features/home/ui/tab_view_pages/all_page/all_page.dart
+++ lib/features/home/ui/tab_view_pages/all_page/all_page.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:grocery_app/core/constants/color_constants.dart';
 import 'package:grocery_app/features/home/bloc/home_bloc.dart';
 import 'package:grocery_app/features/home/models/home_product_data_model.dart';
 import 'package:grocery_app/features/home/ui/widgets/product_display_container/product_display_container.dart';
